8a5686e489293174cd987bfa1d50aa4096527958
Jin Tian <today.xa@gmail.com>
2015-11-11 12:15:01 +0800

    把程序转换到BS结构

--- frt_rest.py
+++ frt_rest.py
@@ -7,14 +7,44 @@ import sys
 import shutil
 import web
 
-
+web.config.debug = False
 
 urls = (
     '/', 'index',
-    '/add', 'add',
     '/getBooking', 'getBooking',
-    '/saveBooking', 'saveBooking'
+    '/saveBooking', 'saveBooking',
+
+    '/getBooking', 'getBooking',
+    '/saveBooking', 'saveBooking',
+
+    '/getBooking', 'getBooking',
+    '/saveBooking', 'saveBooking',
+
+    '/getBooking', 'getBooking',
+    '/saveBooking', 'saveBooking',
+
+    '/getBooking', 'getBooking',
+    '/saveBooking', 'saveBooking',
+
+    "/ping", "ping",
+    "/count", "count",
+    "/reset", "reset"
 )
+
+class ping:
+    def GET(self):
+        return "flag:'successful'"
+
+
+class count:
+    def GET(self):
+        session.count += 1
+        return str(session.count)
+
+class reset:
+    def GET(self):
+        session.kill()
+        return ""
 
 class getBooking:
     def POST(self):
@@ -26,10 +56,7 @@ class getBooking:
         #filename = i.filename
         print filename
         json_str = '[]'
-        if(os.path.exists(filename)):
-            f = codecs.open( filename,'r','utf-8')
-            json_str = f.read()
-            f.close()  
+        json_str = readJson(filename) 
         return json_str
 
 class saveBooking:
@@ -40,24 +67,33 @@ class saveBooking:
         json_obj = json.loads( data )
         booking_date = json_obj["BookingDate"]
         filename = "data/booking/booking_"+ booking_date +".json" 
-        f = codecs.open( filename,'w')
-        f.write(data)
-        f.close() 
+        writeJson(filename, data):
         return "flag:'successful'"
 
     def GET(self):
         return "[]"
 
-class add:
-    def POST(self):
-        i = web.input()
-        json_data = i.json_data
-        #n = db.insert('todo', title=i.title)
-        return "Welcome to FRT!" + json_data
+def writeJson(filename, data):
+    if( not os.path.exists(filename) ):
+        makeFile(filename)
 
-    def GET(self):
-    	#return "Welcome to FRT! GET"
-        raise web.seeother('/')
+    f = codecs.open( filename,'w')
+    f.write(data)
+    f.close() 
+    return "flag:'successful'"
+
+def readJson(filename):
+    json_str = ""
+    if(os.path.exists(filename)):
+        f = codecs.open( filename,'r','utf-8')
+        json_str = f.read()
+        f.close()
+    else
+        json_str = "flag:'error', error_no=1,msg:'file not exists.'"
+    return json_str
+
+
+
 
 
 class index:
@@ -65,5 +101,7 @@ class index:
         return "Welcome to FRT!"
 
 if __name__ == "__main__":
+    
     app = web.application(urls, globals())
+    session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'count': 0})
     app.run()
